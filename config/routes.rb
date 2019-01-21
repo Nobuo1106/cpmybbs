f9415b4078865708e73445b6f30773e21d250809
Iga <gonnbe1106@yahoo.co.jp>
2019-01-21 17:20:20 +0900

    bones are built

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
-    get 'topics/index' => 'topics#index'
+
+    root :to => 'top#home'
+    get 'topics/index' => 'topics#index',　as: :topics
     get 'topics/show:id' => 'topics#show', as: :topics_show
     get 'topics/about' => 'topics#about', as: :about
     get 'topics/contact' => 'topics#contact', as: :contact
-    post 'topics/create' => 'topics#create',  as: :post_create
+    post 'topics/create' => 'topics#create', as: :topics_create
+    post 'post/create' => 'post#create',  as: :post_create
     delete 'topics/delete/:id' => 'topics#delete', as: :topic_delete
 end
